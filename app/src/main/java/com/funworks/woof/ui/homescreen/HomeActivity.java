26cad8110a2f7f61d8d3b6030b4f3a2f3e609336
Rahul-Hariharan <rahul.hariharan2@cognizant.com>
2018-08-08 23:46:38 +0530

    Random home icon
    1. Includes a random home icon.

--- app/src/main/java/com/funworks/woof/ui/homescreen/HomeActivity.java
+++ app/src/main/java/com/funworks/woof/ui/homescreen/HomeActivity.java
@@ -2,12 +2,17 @@ package com.funworks.woof.ui.homescreen;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.funworks.woof.R;
 import com.funworks.woof.ui.mainscreen.MainActivity;
+
+import java.util.Random;
 
 import javax.inject.Inject;
 
@@ -26,11 +31,18 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         HomeScreenViewModel viewModel
                 = ViewModelProviders.of(this).get(HomeScreenViewModel.class);
+        ((ImageView)findViewById(R.id.image)).setImageDrawable(getDrawable(generateRandomIcon()));
     }
 
     public void navigateToQuiz(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private int generateRandomIcon() {
+        int[] image_ids = {R.drawable.home_icon_1, R.drawable.home_icon_2, R.drawable.home_icon_3};
+        Random random = new Random();
+        return image_ids[random.nextInt(3)];
     }
 }
